44ac3113e8d36de460f9962643605fcfb3e708b1
siddhant_prakash <siddhantprakash121@gmail.com>
2023-07-03 13:01:22 +0530

    2 sum

---
+++ 1.two-sum.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
