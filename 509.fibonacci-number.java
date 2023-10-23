6076f40e5a172ec96a0758c66d8e0392ddea29a5
siddhant_prakash <siddhantprakash121@gmail.com>
2023-10-23 22:30:24 +0530

    done

---
+++ 509.fibonacci-number.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=509 lang=java
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+class Solution {
+    public int fib(int n) {
+        
+    }
+}
+// @lc code=end
+
